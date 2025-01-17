5abe1318799941d060efec0e8342176ae8718719
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-01-17 09:34:36 -0800

    project framework

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.use(express.static("public")); // public is where static resources will be found
+router.use("/css", express.static(__dirname + "public/css")); // routes that contain /css will find it in the public/css location
+router.use("/js", express.static(__dirname + "public/js")); // routes that contain /js will find it in the public/js location
+router.use("/images", express.static(__dirname + "public/images")); // routes that contain /images will find it in the public/images location
 
 module.exports = router;
-
-
-
--- server.js
+++ server.js
@@ -1,30 +1,42 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs"); // declares ejs will be the view engine - by default looks for views stored in a views folder
+app.use(expressLayouts); // use the express-ejs-layouts (in the expressLayouts variable)
+app.set("layout", "./layouts/layout"); // not at views root - basic template for view will be in a layouts folder and template name of layout
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
+// Index route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
