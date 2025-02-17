193846a5088c5ddc425675b6a060797acf7e312b
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-02-16 22:29:15 -0800

    consolidate detail controller and routes

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,4 +19,20 @@ invCont.buildByClassificationId = async function (req, res, next) {
   });
 };
 
+/* ***************************
+ *  Build item view
+ * ************************** */
+invCont.buildByInvId = async function (req, res, next) {
+  const item_id = req.params.invId;
+  const data = await invModel.getItemByInvId(item_id);
+  const details = await utilities.buildItemView(data);
+  let nav = await utilities.getNav();
+  const itemName = `${data.inv_year} ${data.inv_make} ${data.inv_model}`;
+  res.render("./inventory/detail", {
+    title: itemName,
+    nav,
+    details,
+  });
+};
+
 module.exports = invCont;
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
-const itemController = require("../controllers/itemController");
 
 // Route to build item view
-router.get("/detail/:invId", itemController.buildByInvId);
+router.get("/detail/:invId", invController.buildByInvId);
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
